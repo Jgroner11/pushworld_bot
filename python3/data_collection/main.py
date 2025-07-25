bf1b5ad94b32985b7859120d1f437b7213ef9cdf
Jacob Groner <jacobgroner@gmail.com>
2025-07-25 16:15:01 -0400

    basic method to learn cscg from a pushworld environment

--- python3/data_collection/main.py
+++ python3/data_collection/main.py
@@ -20,34 +20,86 @@ import matplotlib.pyplot as plt
 from pushworld.puzzle import NUM_ACTIONS
 from pushworld.gym_env import PushWorldEnv
 
+from chmm_actions import CHMM
+
 from absl import app
 
 matplotlib.use('TkAgg')
 
 
+class IntEncoder:
+    def __init__(self, max_size=np.inf):
+        self._size = 0
+        self._map = {}
+        self.max_size = max_size
+
+    def encode(self, x):
+        if x in self._map:
+            return self._map[x]
+        elif self._size == self.max_size:
+            raise Exception("Encoder ran out of unique mappings")
+        else:
+            self._map[x] = self._size
+            self._size += 1
+        
+        
+    def encode_array(self, arr):
+        r = np.array(dtype=np.int64)
+        for x in arr:
+            r.append(self.encode(x))
+        return r
+    
+
 def main(argv):
+    seq_len = 10
+    n_obs = 10
     # Choose puzzle
     path = "benchmark/puzzles/manual/actor_only.pwp"
+
+
     project_root = Path(__file__).resolve().parents[2]
 
+    
     # Create gym environment
-    env = PushWorldEnv(str(project_root / path))
+    env = PushWorldEnv(str(project_root / path), border_width=1, pixels_per_cell=5)
     
     # Reset the environment and show observation
+    encoder = IntEncoder(max_size=n_obs)
+    o = np.zeros((seq_len), dtype=np.int64)
+    a = np.zeros(seq_len, dtype=np.int64)
+
+
     image, info = env.reset()
-    plt.figure(figsize=(5, 5))
-    plt.imshow(image)
-    plt.ion()
-    plt.show()
+    
+    # plt.figure(figsize=(5, 5))
+    # plt.imshow(image)
+    # plt.ion()
+    # plt.show()
+
 
     # Randomly take 10 actions and show observation
-    for _ in range(10):
-        rets = env.step(np.random.randint(NUM_ACTIONS))
-        image = rets[0]
-        plt.imshow(image)
-        plt.draw()
-        plt.pause(0.5)
+    for i in range(seq_len):
+        action = np.random.randint(NUM_ACTIONS)
 
+        o[i] = encoder.encode(image)
+        a[i] = action
+
+        rets = env.step(action)
+
+        image = rets[0]
+
+        # plt.imshow(image)
+        # plt.draw()
+        # plt.pause(0.01)
+
+    n_clones = np.ones(n_obs, dtype=np.int64) * 1
+
+
+    chmm = CHMM(n_clones=n_clones, pseudocount=2e-3, x=o, a=a, seed=42)  # Initialize the model
+    progression = chmm.learn_em_T(o, a, n_iter=100)  # Training
+
+    chmm.pseudocount = 0.0
+    chmm.learn_viterbi_T(o, a, n_iter=100)
 
 if __name__ == '__main__':
     app.run(main)
