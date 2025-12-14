22113f420f8293fced22053bd5ed6ab2c57e6a0a
Jacob Groner <jacobgroner@gmail.com>
2025-12-14 18:35:15 -0500

    updated config file for exp, training works.

--- training/main.py
+++ training/main.py
@@ -5,15 +5,15 @@ experiment_name = "exp"
 # Before running, set your parameters in the config file in this directory. You can see all possible parameters in the schem.yaml file in this directory
 config_path = "config.yaml"
 
-# exp = Experiment(config_path, name=experiment_name, overwrite=True)
-# exp.run()
+exp = Experiment(config_path, name=experiment_name, overwrite=True)
+exp.run()
 
 
-# from experiment import ExperimentData
-# from cscg_helpers import Plotting
+from experiment import ExperimentData
+from cscg_helpers import Plotting
 
-# e = ExperimentData.load(experiment_name=experiment_name)
-# Plotting.plot_graph(e.chmm, e.x, e.a, output_file=f"../experiments/{experiment_name}/cscg.png")
+e = ExperimentData.load(experiment_name=experiment_name)
+Plotting.plot_graph(e.chmm, e.x, e.a, output_file=f"../experiments/{experiment_name}/cscg.png")
 
 from analysis import Analysis
 
